a1d6d7f124199434ae0f4838b8883aa81c2e878a
laeticiaelk <laeticiaelkechai@gmail.com>
2025-07-07 11:47:14 +0200

    fix error var

--- backend.tf
+++ backend.tf
@@ -4,11 +4,9 @@ terraform {
     resource_group_name  = "rg-terraform-state"
     storage_account_name = "stterraformstatebackend"
     container_name       = "tfstate"
-    key                  = "devops-cicd.${ENVIRONMENT}.tfstate"
-    
-    # Ces valeurs peuvent être définies via les variables d'environnement :
+    # key sera passé dynamiquement par -backend-config
+  }
+}
     # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
     # Remplacer ${ENVIRONMENT} par la variable d'environnement ou le workspace Terraform
     # Par exemple, lancez terraform avec: ENVIRONMENT=staging terraform init
-  }
-}
