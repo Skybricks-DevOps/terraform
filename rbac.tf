8c41aee18513ca79297a4a2487adb11c22b552fb
Lamiaa78 <100521392+Lamiaa78@users.noreply.github.com>
2025-07-08 06:38:18 +0200

    delete vault

--- rbac.tf
+++
@@ -1,6 +0,0 @@
-# Role assignment pour permettre à AKS d'accéder aux secrets dans Key Vault
-resource "azurerm_role_assignment" "aks_keyvault_secrets" {
-  scope                = azurerm_key_vault.main.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
-}
