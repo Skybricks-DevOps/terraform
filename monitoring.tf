6c01fd93124ed88d1a020ef256e408025777440b
laeticiaelk <laeticiaelkechai@gmail.com>
2025-09-11 13:33:44 +0200

    rm loganalytics in monitoring.tf

--- monitoring.tf
+++ monitoring.tf
@@ -1,12 +1,3 @@
-# Log Analytics Workspace
-resource "azurerm_log_analytics_workspace" "main" {
-  name                = "log-analytics-${var.environment}"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
 # Azure Monitor Workspace (Prometheus géré)
 resource "azurerm_monitor_workspace" "main" {
   name                = "monitor-workspace-${var.environment}"
