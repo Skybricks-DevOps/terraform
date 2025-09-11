cd037ca7479d3dafdac2216554c2b2c5bfe7fc4b
laeticiaelk <laeticiaelkechai@gmail.com>
2025-09-11 14:51:32 +0200

    fix error

--- provider.tf
+++ provider.tf
@@ -6,8 +6,10 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes_host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
-  kubernetes_client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
-  kubernetes_client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
-  kubernetes_cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
+  kubernetes = {
+    host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
+  }
 }
