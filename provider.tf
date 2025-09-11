15a81bf4ed41c8db2312b1ab3ffcb8176bfe8552
laeticiaelk <laeticiaelkechai@gmail.com>
2025-09-11 14:40:38 +0200

    rm required providers

--- provider.tf
+++ provider.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.0.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
