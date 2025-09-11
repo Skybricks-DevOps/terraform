0ed6847bca0f8d5be8cecd4dc2ece82cd2517710
laeticiaelk <laeticiaelkechai@gmail.com>
2025-09-11 14:44:59 +0200

    fix synatxe

--- provider.tf
+++ provider.tf
@@ -6,10 +6,8 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
-  }
+  kubernetes_host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
+  kubernetes_client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
+  kubernetes_client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
+  kubernetes_cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
 }
